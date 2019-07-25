1168313b35a2cc8fb040855712b69063cf9b5d64
David-Halsey <dartndav@aol.com>
2019-07-25 11:37:14 -0500

    Retrieve a single user by _id

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,12 +4,14 @@ var router = express.Router();
 var Users = require('../../models/users');
 
 
-router.get('/', function (req, res, next) {
-    Users.find({}, function (err, users) {
+router.get('/:userId', function (req, res) {
+
+    var userId = req.params.userId;
+    Users.findOne({ '_id': userId }, function (err, user) {
         if (err) {
             return res.json({ 'success': false, 'error': err });
         }
-        return res.json({ 'success': true, 'users': users });
+        return res.json({ 'success': true, 'user': user });
     });
 });
 
