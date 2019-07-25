83e35d8ca137930b8586c5f169ff5f0366517a4d
David-Halsey <dartndav@aol.com>
2019-07-25 13:54:13 -0500

    Import session and passport packages

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
+//~line 7 after mongoose
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var config = require('./config.dev');
 //~line 10
 var apiUsersRouter = require('./routes/api/users');
-
-
-var mongoose = require('mongoose');
 
 //Test the file
 // console.log(config);
